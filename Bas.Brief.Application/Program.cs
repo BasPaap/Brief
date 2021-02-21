35d9a6f79407e94037f740ba91250436dc6e13ad
Bas Paap <paap@kissthefrog.nl>
2021-02-21 22:01:52 +0100

    Add projects and solution.

---
+++ Bas.Brief.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bas.Brief.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
