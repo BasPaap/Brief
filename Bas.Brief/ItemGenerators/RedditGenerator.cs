175e9c31caa93a18b6aeeec1d8621099942d1648
Bas Paap <paap@kissthefrog.nl>
2021-02-22 23:32:22 +0100

    Implement reddit generator.

--- Bas.Brief/ItemGenerators/RedditGenerator.cs
+++ Bas.Brief/ItemGenerators/RedditGenerator.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Bas.Brief.ItemGenerators
 {
     public sealed class RedditGenerator : ItemGenerator
     {
+        private record RedditItem
+        {
+            public string Title { get; init; }
+            public string Url { get; init; }            
+        }
+
         public RedditGenerator(IEnumerable<KeyValuePair<string, string>> parameters, string content)
             : base(parameters, content)
         {
@@ -15,7 +23,42 @@ namespace Bas.Brief.ItemGenerators
 
         public override async Task<string> ToHtmlAsync()
         {
-            return string.Empty;
+            var redditItem = await GetRedditItemAsync();
+
+            var stringBuilder = new StringBuilder();
+
+            if (!string.IsNullOrWhiteSpace(Content))
+            {
+                stringBuilder.Append($"{Content} ");
+            }
+                                   
+            stringBuilder.Append($"<a href=\"{redditItem.Url}\"><strong>{redditItem.Title}</strong></a><br />");
+            
+            return stringBuilder.ToString();            
+        }
+
+        private async Task<RedditItem> GetRedditItemAsync()
+        {
+            var jsonResponse = await GetJsonAsync();
+
+            var jsonDocument = JsonDocument.Parse(jsonResponse);
+            var dataProperty = jsonDocument.RootElement.GetProperty("data").GetProperty("children")[0].GetProperty("data");
+            var redditItem = new RedditItem
+            {
+                Title = dataProperty.GetProperty("title").GetString(),
+                Url = $"https://reddit.com{dataProperty.GetProperty("permalink").GetString()}"
+            };
+            return redditItem;
+        }
+
+        private async Task<string> GetJsonAsync()
+        {
+            string url = $"https://www.reddit.com/r/{Parameters["sub"]}/top.json?limit={Parameters["numItems"]}&t={Parameters["period"]}";
+
+            using var httpClient = new HttpClient();
+            var jsonResponse = await httpClient.GetStringAsync(url);
+
+            return jsonResponse;
         }
     }
 }
