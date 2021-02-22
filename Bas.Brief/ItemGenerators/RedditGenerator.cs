888ea5c3c5640066f7f02c63a3d1427ed88ba118
Bas Paap <paap@kissthefrog.nl>
2021-02-22 10:11:38 +0100

    Add more ItemGenerators.

---
+++ Bas.Brief/ItemGenerators/RedditGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bas.Brief.ItemGenerators
+{
+    public sealed class RedditGenerator : ItemGenerator
+    {
+        public RedditGenerator(IEnumerable<KeyValuePair<string, string>> parameters, string content, bool isFirst, bool isLast)
+            : base(parameters, content, isFirst, isLast)
+        {
+        }
+
+        public override string ToHtml()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
