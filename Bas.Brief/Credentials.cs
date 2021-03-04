2fe222841cde5308b29d29ce54a66deef8472a30
Bas Paap <paap@kissthefrog.nl>
2021-03-04 13:45:29 +0100

    Add extra entropy to encryption via unique id.

--- Bas.Brief/Credentials.cs
+++ Bas.Brief/Credentials.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -14,28 +15,55 @@ namespace Bas.Brief
 
         public byte[] EncryptedPasswordBytes { get; set; }
 
+        private byte[] semiUniqueId;
+
         public Credentials()
         {
+            var executableDrive = new DriveInfo(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(RuntimeInformation.OSArchitecture);
+            stringBuilder.Append(RuntimeInformation.ProcessArchitecture);
+            stringBuilder.Append(Environment.MachineName);
+            stringBuilder.Append(Environment.ProcessorCount);
+            stringBuilder.Append(Environment.UserName);
+            stringBuilder.Append(executableDrive.DriveFormat);
+            stringBuilder.Append(executableDrive.DriveType);
+            stringBuilder.Append(executableDrive.Name);
+            stringBuilder.Append(executableDrive.RootDirectory);
+            stringBuilder.Append(executableDrive.TotalSize);
+
+            semiUniqueId = new byte[stringBuilder.Length * sizeof(char)];
+            System.Buffer.BlockCopy(stringBuilder.ToString().ToCharArray(), 0, semiUniqueId, 0, semiUniqueId.Length);
         }
 
-        public Credentials(string userName, string password)
+        public Credentials(string userName, string password) 
+            : this()
         {
             UserName = userName;
 
             var passwordBytes = new byte[password.Length * sizeof(char)];
             System.Buffer.BlockCopy(password.ToCharArray(), 0, passwordBytes, 0, passwordBytes.Length);
 
+            
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                EncryptedPasswordBytes = ProtectedData.Protect(passwordBytes, null, DataProtectionScope.CurrentUser);
+                EncryptedPasswordBytes = ProtectedData.Protect(passwordBytes, semiUniqueId, DataProtectionScope.CurrentUser);
+            }
+            else
+            {
+                using Aes aes = Aes.Create();
+                
             }
         }
 
         public string GetDecryptedPassword()
         {
+            using Aes aes = Aes.Create();
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                var decryptedPasswordBytes = ProtectedData.Unprotect(EncryptedPasswordBytes, null, DataProtectionScope.CurrentUser);
+                var decryptedPasswordBytes = ProtectedData.Unprotect(EncryptedPasswordBytes, semiUniqueId, DataProtectionScope.CurrentUser);
                 char[] characters = new char[decryptedPasswordBytes.Length / sizeof(char)];
                 System.Buffer.BlockCopy(decryptedPasswordBytes, 0, characters, 0, decryptedPasswordBytes.Length);
                 return new string(characters);
